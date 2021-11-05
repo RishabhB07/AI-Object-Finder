522261096933543315d75f37f0f8dc51f65e4545
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-11-05 20:18:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+status="";
 function setup(){
-    canvas=createCanvas(300, 300);
+    canvas=createCanvas(290, 290);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
 }
 function draw(){
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 290, 290);
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("model loaded!");
+    status=true;
 }
