74c5f7a7a019786b435887eb9162b1b7410c1332
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-11-07 00:03:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 status="";
+objects=[];
 function setup(){
-    canvas=createCanvas(290, 290);
-    canvas.center();
-    video=createCapture(VIDEO);
-    video.hide();
+  canvas=createCanvas(290, 290);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.hide();
 }
 function draw(){
     image(video, 0, 0, 290, 290);
+    if(status!=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            fill("#00FF00");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#00FF00");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label==object_name){
+              document.getElementById("object_status").innerHTML=object_name+" found";
+              synth=window.speechSynthesis;
+              utterThis=new SpeechSynthesisUtterance(object_name+"found");
+              synth.speak(utterThis);
+            }
+            else{
+              document.getElementById("object_status").innerHTML=object_name+" not found";
+              synth=window.speechSynthesis;
+              utterThis=new SpeechSynthesisUtterance(object_name+"not found");
+              synth.speak(utterThis);
+            }          
+        }
+    }
 }
 function start(){
-    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML="Status : Detecting Objects";
-    object_name = document.getElementById("object_name").value;
+  objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML="Status : Detecting Objects";
+  object_name=document.getElementById("object_name").value;
 }
 function modelLoaded(){
-    console.log("model loaded!");
-    status=true;
+  console.log("model loaded!")
+  status = true;
+}
+function gotResult(error, results){
+  if (error){
+    console.log(error);
+  }
+  else{
+  console.log(results);
+  objects = results;
+  }
 }
